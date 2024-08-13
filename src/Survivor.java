1b17dde5ea6a096ee8b72539422081ba900b4cb9
Slim Jim <youssefibrahim909@gmail.com>
2024-08-13 16:08:18 -0500

    added survivor parent class

---
+++ src/Survivor.java
@@ -0,0 +1,36 @@
+
+public class Survivor {
+    
+
+    private int health;
+    private int damage;
+
+    public Survivor(int health, int damage){
+        this.health = health;
+        this.damage =damage;
+
+    }
+
+    public int attack(){
+        return this.damage;
+    }
+
+    public void hit(int damageTaken){
+        this.health -= damageTaken;
+    }
+
+    public boolean dead(){
+        return this.health <=0;
+
+    }
+
+    public int getHealth(){return this.health;}
+
+    public void setHealth(int health){this.health = health;}
+
+    public int getDamage(){return this.damage;}
+
+    public void setDamage(int damage){this.damage = damage;}
+}
+
+
