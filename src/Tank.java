94df40343c918c9566c04e1a32402c3ebad79c1e
wahlersj <150095256+wahlersj@users.noreply.github.com>
2024-08-13 20:33:52 -0500

    Update Tank.java

--- src/Tank.java
+++ src/Tank.java
@@ -3,21 +3,8 @@ CSC 422 - Software Engineering
 Zombie War Project
 */
 
-public class Zombie extends Character {
-
-/*Sets the "Type" of Zombie*/
-    static final String COMMON_INFECTED = "CommonInfected";
-    static final String TANK = "Tank";
-
-    private final String zombieType;
-
-    public Zombie(String zombieType, int health, int attack) {
-        super(Character.Zombie, health, attack);
-        this.zombieType = zombieType;
-}
-
-    @Override
-    public String toString() {
-        return zombieType;
+public class Tank extends Zombie {
+    public Tank()
+        super(100,10);
     }
 }
