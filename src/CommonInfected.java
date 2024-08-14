41396990dc379fa8bb3b30ea0fd6de00f5ffcb2b
wahlersj <150095256+wahlersj@users.noreply.github.com>
2024-08-13 20:35:03 -0500

    Update CommonInfected.java

--- src/CommonInfected.java
+++ src/CommonInfected.java
@@ -3,12 +3,7 @@ CSC 422 - Software Engineering
 Zombie War Project
 */
 public class CommonInfected extends Zombie {
-
-    private static final int HEALTH = 30;
-    private static final int ATTACK = 5;
-
-
-    public CommonInfected() {
-        super(Zombie.COMMON_INFECTED, HEALTH, ATTACK);
+    public CommonInfected()
+        super(30,5);
     }
 }
