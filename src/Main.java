32f74a8c25f518db95aabafd62125920cd3d93d2
wahlersj <150095256+wahlersj@users.noreply.github.com>
2024-08-16 01:47:31 +0000

    Added the ZombieWar battle to the main class.

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
                 // generating a number between 0 and 1
                 int num = random.nextInt(2);
-                
+
                 switch (num) {
 
                     case 0 -> characters.add(new CommonInfected());
@@ -49,7 +49,7 @@ public class Main {
         }else{
 
             for (int i = 0; i < count; i++){
-                
+
                 // generating a random number between 0 and 3 to generate a random survivor
                 int num = random.nextInt(3);
 
@@ -64,8 +64,52 @@ public class Main {
 
             return characters;
         }
-    
+
     }
 
 
 }
+
+    //Starting the Zombie War
+    public static void ZombieWar(List < Object > survivors, List < Object > zombies) {
+
+        //creating a new random object for selecting a random survivor or zombie
+        Random random = new Random();
+
+        //While loop to keep the game going until one side is empty
+        while (!survivors.isEmpty() && !zombies.isEmpty()) {
+
+            //Generatates a random number between 0 and the size of the list
+            Object survivor = survivors.get(random.nextInt(survivors.size()));
+
+            Object zombie = zombies.get(random.nextInt(zombies.size()));
+
+            //Survivor attacks the zombie
+            //Zombie attacks the suvivor
+            int survivorAttack = ((Survivor) survivor).attack();
+
+            int zombieAttack = ((Zombie) zombie).attack();
+
+            //Takes damage lose health
+            ((Zombie) zombie).hit(survivorAttack);
+
+            ((Survivor) survivor).hit(zombieAttack);
+
+            //If the zombie is dead it is removed from the list
+            if (((Zombie) zombie).dead()) {
+
+                System.out.println(((Survivor) survivor).getClass().getSimpleName() + " killed " + ((Zombie) zombie).getClass().getSimpleName());
+
+                zombies.remove(zombie);
+                }
+
+            //If the Survivor is dead it is removed from the list
+            if (((Survivor) survivor).dead()) {
+
+            System.out.println(((Zombie) zombie).getClass().getSimpleName() + " killed " + ((Survivor) survivor).getClass().getSimpleName());
+
+            survivors.remove(survivor);
+
+            }    
+        }    
+    }
